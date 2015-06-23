c8a06cb2069b2ff16497b064d4d44e3cb852c031
overallduka <overallduka@hotmail.com>
2015-06-23 14:27:17 -0300

    Added account_user assets and fixed page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,11 +1,14 @@
 class PagesController < ApplicationController
 
+  before_action :authenticate_user!, only: [:user_account]
+
   def index
 
   end
 
   def user_account
 
+    render :layout => false
   end
 
 end
